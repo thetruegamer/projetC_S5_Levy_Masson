2f1a0fb1830519f59e8094ed1868a5a6ee16bb95
bastien <bastienmasson2@laposte.net>
2016-12-02 16:28:21 +0100

    formatage nombre finie

--- formatage.c
+++ formatage.c
@@ -1,19 +1,42 @@
 #include "header.h"
 
+//Convertit un nombre en chaine de 4 chars et complete par des zeros devant si besoin
 char *formatageNb(int nombre){
 	int longueur = 0;
-	while(nombre){
-		nombre /= 10;
+	int pivot = nombre;
+	char *resultat = malloc(4*sizeof(char));
+	char *zero = NULL;
+
+	while(pivot){
+		pivot /= 10;
 		longueur++;
 	}
+
+	zero = malloc((4-longueur)*sizeof(char));
+
 	if(longueur == 0)
 		longueur++;
-	printf("l : %d\n", longueur);
-	return "";
+	if(longueur > 4){
+		printf("Nombre trop grand !\n");
+		exit(1);
+	} else if(longueur == 4){
+		sprintf(zero, "%d", nombre);
+	} else {
+		for(int i = 0; i < 4-longueur; i++)
+			strcat(zero,"0");
+		sprintf(resultat, "%d", nombre);
+		strcat(zero, resultat);
+	}
+
+	return zero;
 }
+
+
 
 int main(int argc, char *argv[])
 {
-	//formatageNb(1);
+	char *test = malloc(4*sizeof(char));
+	test = formatageNb(690);
+	printf("test : %s\n", test);
 	return 0;
 }
